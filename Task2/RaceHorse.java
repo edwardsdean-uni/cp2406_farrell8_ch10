c6703218b8f08d70b923cddee32f29f101cb3017
edwardsdean <edwardsdean@hotmail.com>
2017-09-11 14:21:03 +1000

    Task2

---
+++ Task2/RaceHorse.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class RaceHorse extends Horse{
+	private String raceNum;
+	
+	public void setRace() {
+		raceNum = JOptionPane.showInputDialog(null,"Please enter number of the horse's races:");
+	}
+
+	public String getRace() {
+		return raceNum;
+	}
+	
+}
